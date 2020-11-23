973ebc7f96b23de664da292a5c3d514f3ae5e17e
pradipantu <pradipantu@hotmail.com>
2020-11-23 22:31:31 +0530

    First Commit

---
+++ src/main/java/com/boot/profiles/ExternalProperties/ExternalPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.boot.profiles.ExternalProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalPropertiesApplication.class, args);
+	}
+
+}
